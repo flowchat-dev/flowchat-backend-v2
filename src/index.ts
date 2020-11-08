e59f6fb91adf15ee43dbb5d92d3b2bb6fcadba0e
rycont <reactdev@kakao.com>
2020-11-08 16:14:33 +0900

    feat : add default router

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { config } from 'dotenv';
+import app from './app';
+
+config();
+app.listen();
---
+++ src/router/index.ts
@@ -0,0 +1,4 @@
+import { Router } from 'express';
+
+const index = Router();
+export default index;
